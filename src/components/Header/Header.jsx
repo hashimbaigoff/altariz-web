599ff6d76258fbce3d7c42abd7dd6e82f1a7b314
syed-kousar-fatima <syedkousar99@gmail.com>
2024-04-16 16:18:06 +0530

    Add: container

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import NavItem from "../NavItem";
 function Header(){
     return(
         <header className="header">
-            <div className="logo"><img src="images/download.png" />
+            <div className="logo"><img src="images/AltarizLogoBlue.jpg" />
             <h3 className="logo-text">Altariz Solutions Pvt Ltd.</h3></div>
             <div className="nav-path">
             <NavItem path="/" label="Home"/>
