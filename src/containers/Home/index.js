ee61507a6ada6432c56c4fc24b496872126290ea
syed-kousar-fatima <syedkousar99@gmail.com>
2024-04-16 11:20:34 +0530

    add: sass

---
+++ src/containers/Home/index.js
@@ -0,0 +1,12 @@
+import Banner from "../../components/Banner";
+import Header from "../../components/Header/Header";
+
+function Home() {
+    return (
+        <>
+            <Banner />
+        </>
+    );
+}
+
+export default Home;
