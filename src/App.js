63b0e98f0bb1b79d506db8f87edec3748ecc6fb2
syed-kousar-fatima <syedkousar99@gmail.com>
2024-04-23 13:05:41 +0530

    add:remove navbar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './containers/Home';
 import Header from './components/Header/Header';
-import Navbar from './components/Navbar/navbar';
 import Footer from './components/Footer/footer';
 import LogandReg from './containers/LoginRegister/index';
 
